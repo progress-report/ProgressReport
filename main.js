340f2b07c184bc0a39217a817893bb2d7c50dc57
LomaxRx <andrewlomaxr@gmail.com>
2017-07-22 15:08:55 -0400

    hover event

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function __$styleInject(css, returnValue) {
 
 mapboxgl = mapboxgl && mapboxgl.hasOwnProperty('default') ? mapboxgl['default'] : mapboxgl;
 
-__$styleInject("#map {\n  position:absolute;\n  top:0;\n  bottom:0;\n  width:100%; \n}\n", undefined);
+__$styleInject("body{\n  padding: 0;\n  margin: 0;\n}\n\n#map {\n  position:absolute;\n  top:0;\n  bottom:0;\n  width:100%;\n}\n", undefined);
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2F6aWxpY28iLCJhIjoiY2o1ZmRlNDE3MGVwMTJ3bnp3amljM2EyNyJ9.a4PkGcsyeqVEudMTnNG8QA';
 var map = new mapboxgl.Map({
@@ -29,6 +29,32 @@ var map = new mapboxgl.Map({
     style: 'mapbox://styles/kazilico/cj5fjeg4m2d6e2rpezbgznnzk'
 });
 
-console.log(map);
+map.on('mousemove', function (e) {
+    var features = map.queryRenderedFeatures(e.point);
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = features[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var f = _step.value;
+
+            if (f.layer['source-layer'] == 'districts') console.log(f.layer);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+});
 
 }(mapboxgl));
--- src/js/Map.js
+++ src/js/Map.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+import hover from './Hover';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2F6aWxpY28iLCJhIjoiY2o1ZmRlNDE3MGVwMTJ3bnp3amljM2EyNyJ9.a4PkGcsyeqVEudMTnNG8QA';
 let map = new mapboxgl.Map({
@@ -6,6 +7,17 @@ let map = new mapboxgl.Map({
     style: 'mapbox://styles/kazilico/cj5fjeg4m2d6e2rpezbgznnzk',
 });
 
-console.log(map);
+let currentDistrict = {};
+map.on('mousemove', function (e) {
+    let features = map.queryRenderedFeatures(e.point);
+    for(let f of features){
+      if(f.layer['source-layer']=='districts'){
+        if(currentDistrict!==f){
+          hover(f);
+          currentDistrict = f;
+        }
+      }
+    }
+});
 
 export default map;
